f99db111c68f86ec9dd1b3d7df1697dd8d806dcc
delkant <delkant@mburukuja.com>
2016-10-25 19:19:31 -0400

    Create Earthquake Model

---
+++ EarthQuakes/Earthquake.swift
@@ -0,0 +1,22 @@
+//
+//  Earthquake.swift
+//  EarthQuakes
+//
+//  Created by delkant on 10/25/16.
+//  Copyright © 2016 rodrigo. All rights reserved.
+//
+
+import Foundation
+
+class Earthquake {
+    
+    let magnitud: Double = 0
+    let time: Date = Date()
+    let url: URL? = nil
+    let place: String = ""
+    let detail: URL? = nil
+    let longitude: Double = 0.0
+    let latitude: Double = 0.0
+    let depth: Double = 0.0
+    
+}
